0a0b5184366061c6e788656b340ef48d2789cfcf
saifhaseeb <smhas12@student.monash.edu>
2017-05-24 18:06:17 +1000

    added notebook widgets to gui

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+
 from tkinter.scrolledtext import ScrolledText
 
 # main base window
+# [depot, ......., capacity]
 
 
 class Base(Frame):
@@ -28,7 +30,19 @@ def centerwindow(w, h):
 
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
+# emtpy modules for design
 
+def heuristic():
+    return 0
+
+def metaheuristic():
+    return 0
+
+def exact():
+    return 0
+
+
+# create base window root and notebook widget instance
 root = Tk()
 root.title('CVRP Algorithms')
 nb = ttk.Notebook(root)
@@ -36,17 +50,28 @@ nb = ttk.Notebook(root)
 # first page, which would get widgets gridded into it
 page1 = ttk.Frame(nb)
 
-# second page
+# adding more pages and titles for them
 page2 = ttk.Frame(nb)
 page3 = ttk.Frame(nb)
-text = ScrolledText(page2)
-text.pack(expand=1, fill="both")
 
 nb.add(page1, text='Clarke-Wright')
 nb.add(page2, text='Tabu Search')
 nb.add(page3, text='Exact Solver')
-
 nb.pack(expand=1, fill="both")
+
+# quit button for entire program
+tkButtonQuit = Button(root, text="Quit", command=quit)
+tkButtonQuit.pack()
+
+# buttons for running algorithms in tabs
+heuristicLaunch = Button(page1, text="Run heuristic algorithm", command=heuristic())
+metaHeuristicLaunch = Button(page2, text="Run metaheuristic algorithm", command=metaheuristic())
+exactSolverLaunch = Button(page3, text="Run exact solver algorithm", command=exact())
+heuristicLaunch.pack()
+metaHeuristicLaunch.pack()
+exactSolverLaunch.pack()
+
+# center the window to middle of screen and run program
 centerwindow(800, 650)
 app = Base(root)
 root.mainloop()
